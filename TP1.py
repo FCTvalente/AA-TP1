a309cefff0967995d5f93ab736b569879d49779f
Pedro <pd.valente@campus.fct.unl.pt>
2019-10-16 16:20:05 +0100

    basics done

--- TP1.py
+++ TP1.py
@@ -1,2 +1,22 @@
 # -*- coding: utf-8 -*-
 
+import numpy as np
+from sklearn.utils import shuffle
+from sklearn import svm
+from sklearn.naive_bayes import GaussianNB 
+from sklearn.neighbors.kde import KernelDensity
+
+traindata = np.loadtxt('TP1_train.tsv')
+shuffle(traindata)
+
+trainYs = traindata[:,-1]
+trainXs = traindata[:,:-1]
+trainXs = (trainXs - np.mean(trainXs, axis=0))/np.std(trainXs, axis=0)
+
+testdata = np.loadtxt('TP1_test.tsv')
+shuffle(testdata)
+
+testYs = testdata[:,-1]
+testXs = testdata[:,:-1]
+testXs = (testXs - np.mean(testXs, axis=0))/np.std(testXs, axis=0)
+
