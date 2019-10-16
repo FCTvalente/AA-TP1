118df35457fe84c56bbbdb7458aa27b4d9ed97e8
Pedro <pd.valente@campus.fct.unl.pt>
2019-10-16 16:35:43 +0100

    svm done maybe

--- TP1.py
+++ TP1.py
@@ -9,6 +9,9 @@ from sklearn.neighbors.kde import KernelDensity
 traindata = np.loadtxt('TP1_train.tsv')
 shuffle(traindata)
 
+svmimage = 'SVM'
+nbimage = 'NB'
+
 trainYs = traindata[:,-1]
 trainXs = traindata[:,:-1]
 trainXs = (trainXs - np.mean(trainXs, axis=0))/np.std(trainXs, axis=0)
@@ -20,3 +23,6 @@ testYs = testdata[:,-1]
 testXs = testdata[:,:-1]
 testXs = (testXs - np.mean(testXs, axis=0))/np.std(testXs, axis=0)
 
+SupportVector = svm.SVC(C = 1, kernel = 'rbf', gamma = 0.2)
+
+SupportVector.fit(trainXs, trainYs)
